fe3948e93a6788da31e1fb13ceb968afa7ffb4cc
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-24 23:32:49 -0400

    rmve js imports and some functions

--- src/greddit-scrapper.go
+++ src/greddit-scrapper.go
@@ -1,23 +1,9 @@
-// import { Buffer } from "buffer";
-// import * as Request from "request-promise-native";
-// import {RequestPromiseOptions} from "request-promise-native";
-// import {
-// 	IRedditCredentials,
-// 	IRequestOptions,
-// 	IAPIUrl,
-// 	IAccessTokenResult,
-// 	IPageListingResult,
-// 	IPageListingResults,
-// } from "./RedditScraper.types";
-
 package gredditScrapper
 
 import (
-	"fmt"
-	"os"
-	"github.com/thejerf/strinterp"
-	"bytes"
 	"time"
+
+	"github.com/thejerf/strinterp"
 )
 
 type GredditScraper struct {
@@ -46,7 +32,7 @@ type GredditScraper struct {
 	// 	private static createAuthToken(options: IRedditCredentials): string {
 	// 		return Buffer.from(`${options.AppId}:${options.AppSecret}`).toString("base64");
 	// 	}
-	
+
 	// 	private async getPage(options: IRequestOptions): Promise<IPageListingResults> {
 
 	// 		const finalUrl: string = `${RedditScraper.API_URL.GetData}${options.SubReddit}/${options.SortType}/`;
@@ -54,7 +40,7 @@ type GredditScraper struct {
 	// 		const requestOptions: RequestPromiseOptions = {
 	// 			json: true,
 	// 			headers: {
-		// 				"Authorization": `Bearer ${this.AccessToken}`,
+	// 				"Authorization": `Bearer ${this.AccessToken}`,
 	// 				"User-Agent": "RedditScraper",
 	// 			},
 	// 		};
@@ -71,12 +57,12 @@ type GredditScraper struct {
 
 	// 		return pageData.data.children;
 	// 	}
-	
+
 	// 	private async getAccessToken(): Promise<string> {
 
-		// 		const options: RequestPromiseOptions = {
-			// 			formData: {
-				// 				grant_type: "client_credentials",
+	// 		const options: RequestPromiseOptions = {
+	// 			formData: {
+	// 				grant_type: "client_credentials",
 	// 				device_id: "DO_NOT_TRACK_THIS_DEVICE",
 	// 			},
 	// 			json: true,
@@ -84,20 +70,20 @@ type GredditScraper struct {
 	// 				Authorization: `Basic ${this.AuthToken}`,
 	// 			},
 	// 		};
-	
+
 	// 		const accessTokenData: IAccessTokenResult = await Request.post(RedditScraper.API_URL.AccessToken, options);
 	// 		return accessTokenData.access_token;
 	// 	}
-	
+
 	// }
 }
 
-func (g GredditScraper) GetAccessToken() string {
+func (g GredditScraper) GetAccess() string {
 	AccessToken := "https://www.reddit.com/api/v1/access_token"
 	return AccessToken
 }
 func (g GredditScraper) createAuthToken(options IRedditCredentials) string {
-	
+
 	// instead of a buffer method to hide appid and other info which is why i think this exists i will try something else the string is in base 64 which I am curious as to why and where this method is used.
 	authToken, err := strinterp.NewInterpolator().InterpStr("%RAW:%RAW", options.AppId, options.AppSecret)
 	if err != nil {
@@ -110,32 +96,46 @@ func (g GredditScraper) GetData() string {
 	GetData := "https://oauth.reddit.com/r/"
 	return GetData
 }
-// 	public async scrapeData(options: IRequestOptions): Promise<IPageListingResults> {
+func listingIsBeforeDateRange(page IPageListingResult, options IRequestOptions) bool {
+	return page.data.created_utc < options.BeforeDate.getTime()
+}
+
+// 			const listingIsBeforeDateRange = (page: IPageListingResult) =>
+// 					(page.data.created_utc < options.BeforeDate.getTime());
 func (g GredditScraper) scrapeData(options IRequestOptions) map[string]string {
 
-			if (!options.BeforeDate) {
-				options.BeforeDate = time.Now(); //probably will break need proper time figure out date function return in js
-			}
+	if !options.BeforeDate {
+		options.BeforeDate = time.Now() //probably will break need proper time figure out date function return in js
+	}
 
 	// 		this.AccessToken = await this.getAccessToken();
-	
+	g.AccessToken = g.GetAccessToken()
 	// 		let finalPageListings: IPageListingResults = [];
 	// 		let recordCount: number = 0;
-	
-	// 		do {
-	// 			const pageData = await this.getPage(options);
-	
-	// 			const listingIsBeforeDateRange = (page: IPageListingResult) =>
-	// 					(page.data.created_utc < options.BeforeDate.getTime());
-	
-	// 			const dataBeforeDateRange = pageData.filter(listingIsBeforeDateRange);
-	
+	recordCount := 0
+	var finalPageListings IPageListingResults
+
+	for options.FullName && recordCount < options.Pages * options.Records {
+		const pageData = g.getPage(options)                                   //need to decide type
+		filter(pageData, listingIsBeforeDateRange) // need to decide type
+		for _, value: range pageData{
+			if pageData.includes(listingIsBeforeDateRange) {
+				// append element at this index to list otherwise continue looping
+			}
+
+		}
+		const dataBeforeDateRange = 
+		finalPageListings = finalPageListings + dataBeforeDateRange
+
+	}
+
 	// 			finalPageListings = finalPageListings.concat(dataBeforeDateRange);
 	// 			options.FullName = this.NextPage;
 	// 			recordCount += options.Records;
-	
+
 	// 		} while (options.FullName && recordCount < options.Pages * options.Records);
-	
+
 	// 		return finalPageListings;
 }
+
 // 	}
